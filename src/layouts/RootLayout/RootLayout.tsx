0c6de7991b3f9625c3e7b3ed8a1828029d27835b
Valeriia Maltseva <lerok.maltseva96@gmail.com>
2024-06-15 15:44:15 +0200

    added styled-components library

--- src/layouts/RootLayout/RootLayout.tsx
+++ src/layouts/RootLayout/RootLayout.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Layout } from 'antd';
+
+import * as S from './styled';
+
+const { Header, Footer, Content } = Layout;
 
 const RootLayout = ({ children }: React.PropsWithChildren) => {
     return (
-        <div>
-            <div>Header</div>
-            <div>{children}</div>
-            <div>Footer</div>
-        </div>
+        <S.Layout>
+            <Header>Header</Header>
+            <Content>{children}</Content>
+            <Footer>Footer</Footer>
+        </S.Layout>
     );
 };
 
---
+++ src/layouts/RootLayout/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+import { Layout as AntLayout } from 'antd';
+
+export const Layout = styled(AntLayout)`
+    height: 100vh;
+`;
